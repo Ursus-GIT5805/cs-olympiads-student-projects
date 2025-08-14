3c56390e82d09c6f4940a9e879a8bb63f0c91035
Ursus Wigger <wigger.ursus@gmail.com>
2025-08-14 10:10:09 +0200

    Added preset models

---
+++ plasticity/model/presets.py
@@ -0,0 +1,37 @@
+import jax
+from model import Model
+
+def FirstResnet_Mnist():
+    params = [
+        linear(784, 100, key),
+        linear(100, 100, key),
+        linear(100, 100, key),
+        linear(100, 10, key),
+    ]
+
+    def run(params, a):
+        a = feedforward_linear(params[0], a)
+
+        x1 = a.copy()
+
+        a = jax.nn.sigmoid(a)
+        a = feedforward_linear(params[1], a)
+        a = batch_norm(a)
+        a = jax.nn.relu(a)
+
+        a = feedforward_linear(params[2], a)
+        a = batch_norm(a)
+
+        a = a + x1
+        a = jax.nn.relu(a)
+
+        a = feedforward_linear(params[3], a)
+        a = jax.nn.softmax( a )
+        return a
+
+    return Model.init(
+        params,
+        jax.jit(run),
+        input_dim=784,
+        output_dim=10,
+    )
