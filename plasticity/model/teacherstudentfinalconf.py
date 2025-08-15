3a70f39333b2a4456a700b117ad40e6183e5fa43
alessandro <ale03bond@gmail.com>
2025-08-15 17:29:56 +0200

    gpu

--- plasticity/model/teacherstudentfinalconf.py
+++ plasticity/model/teacherstudentfinalconf.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
         print(current_student_epochs)
         model_student_along.train(
             random_noise_step, train_student_y,
-            epochs=student_epochs, batch_size=batch_size,
+            epochs=current_student_epochs, batch_size=batch_size,
             optimizer = optax.sgd(learning_rate=0.1),
             #return_score=True,
             #evaluate=(test_x, test_y),
