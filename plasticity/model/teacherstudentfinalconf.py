4bba7120fe52b4c7e3b55ad9ae54a9f1fe3849d0
alessandro <ale03bond@gmail.com>
2025-08-17 15:41:11 +0200

    sus gpu

--- plasticity/model/teacherstudentfinalconf.py
+++ plasticity/model/teacherstudentfinalconf.py
@@ -231,7 +231,8 @@ if __name__ == '__main__':
             random_noise_step, train_student_y,
             epochs=student_epochs, batch_size=batch_size,
             optimizer = optax.sgd(learning_rate=0.1),
-            
+            p_slow=0.05,
+            gamma=0.9
             #return_score=True,
             #evaluate=(test_x, test_y),
         )
