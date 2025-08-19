ef1ef37907a3739d7b666e7c214684e64afa56cd
alessandro <ale03bond@gmail.com>
2025-08-19 10:19:23 +0200

    sus gpu

--- plasticity/model/presets.py
+++ plasticity/model/presets.py
@@ -6,6 +6,65 @@ def get_dead(a):
     return jnp.sum(abs(a)<1e-5)
 
 
+def Resnet4_mnist(key):
+    k1,k2,k3,k4 = jax.random.split(key, 4)
+
+    params = [
+        linear(784, 100, k1),
+        linear(100, 100, k2),
+        linear(100, 100, k3),
+        linear(100, 10, k4),
+    ]
+
+    def run(params, a):
+        a = feedforward_linear(params[0], a)
+
+        x1 = a.copy()
+
+        a = jax.nn.sigmoid(a)
+        a = feedforward_linear(params[1], a)
+        a = batch_norm(a)
+        a = jax.nn.leaky_relu(a,0.01)
+
+        a = feedforward_linear(params[2], a)
+        a = batch_norm(a)
+
+        a = a + x1
+        a = jax.nn.leaky_relu(a,0.01)
+
+        a = feedforward_linear(params[3], a)
+        a = jax.nn.softmax( a )
+        return a
+
+    def get_dead_units(params, a):
+        deads = 0
+        a = feedforward_linear(params[0], a)
+        x1 = a.copy()
+        a = jax.nn.sigmoid(a)
+        deads+=get_dead(a)
+        a = feedforward_linear(params[1], a)
+        a = batch_norm(a)
+        a = jax.nn.relu(a)
+        deads+=get_dead(a)
+        a = feedforward_linear(params[2], a)
+        a = batch_norm(a)
+
+        a = a + x1
+        a = jax.nn.relu(a)
+        deads+=get_dead(a)
+        a = feedforward_linear(params[3], a)
+        a = jax.nn.softmax( a )
+        deads+=get_dead(a)
+        return deads
+    return Model.init(
+        params,
+        jax.jit(run),
+        jax.jit(get_dead_units),
+        input_dim=784,
+        output_dim=10,
+    )
+
+
 
 def Resnet1_mnist(key):
     k1,k2,k3,k4 = jax.random.split(key, 4)
